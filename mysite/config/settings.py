41a6cd80f91d8372c83d8d61b7a707bc85ded20d
yosukeito.ind <yousuke08@me.com>
2021-04-20 16:19:22 +0900

    [add] pipenvでwakatimeをインストール，SecretKeyを変更した

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+# local_settingsを読み込み
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v+y*rlemz%dg^+@!a5agy)!u64mj6#h=#)uxt4c$mus-nnvedz'
+# local_settingsに記述
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
