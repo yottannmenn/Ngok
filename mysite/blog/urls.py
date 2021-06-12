839e454df1313fb749a14635b1fa269e6cd884c1
yosukeito.ind <yousuke08@me.com>
2021-06-13 01:40:58 +0900

    [add] 記事一覧を表示するblog_listを作成
    記事一覧を表示するblog_listを作成した
    Postモデルの中身を一覧で表示する

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from . import views
+from .models import Post
 
 app_name = 'blog'
 urlpatterns = [
     path("", TemplateView.as_view(template_name="blog/index.html"), name="index"),
+    path('post_list/', ListView.as_view(model=Post), name='post_list'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,6 @@
+from typing import List
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+from django.views.generic import ListView
