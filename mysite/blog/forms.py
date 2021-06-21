1f5a918a87c6f7f0fc10c33de6f8bcc648c49e0e
yosukeito.ind <yousuke08@me.com>
2021-06-21 23:17:13 +0900

    [update] 記事をマークダウンで書けるように変更
    記事をマークダウンで書けるように変更した
    criateのフォームから投稿できる

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from .models import Post
 
+from markdownx.widgets import MarkdownxWidget
+
 
 class PostCreateForm(forms.ModelForm):  # DjangoのModelFormでは強力なValidationを使える
 
     class Meta:
         model = Post  # Post モデルと接続し、Post モデルの内容に応じてformを作ってくれる
         fields = ('title', 'text')  # 入力するカラムを指定
+        widgets = {
+            'text': MarkdownxWidget(),
+        }
